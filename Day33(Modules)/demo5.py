d546bd3cd6d31b0d35ad1bfae6294f9db79b69d2
rekha <rekhakirani@gmail.com>
2025-04-15 13:26:48 +0530

    Added a new print

--- Day33(Modules)/demo5.py
+++ Day33(Modules)/demo5.py
@@ -12,7 +12,7 @@ prod_num = basicArithmetic.multiplication(20,60)
 print(prod_num)
 # Product of 20 and 60 is 1200
 
-
+print()
 div_num = basicArithmetic.division(40,3)
 print(div_num)
 # Quotient of 40 and 3 is 13.333333333333334
