7bf8be0fa9fe6fa9937c553c3a98efdac36d5ba6
rekha <rekhakirani@gmail.com>
2025-04-15 13:30:20 +0530

    Added a new print

--- Day19(ForLoopContd)/demo4.py
+++ Day19(ForLoopContd)/demo4.py
@@ -1,4 +1,17 @@
 # Print all positive numbers in a pos_list and negative numbers in another list neg_list
 list1 = [10,-20,40,-55,30,-35,-19,100,60.5]
 
+num = 6
+is_prime = False
+for i in range(2,num):
+	if num%i == 0:
+		is_prime = False
+		break
+	else:
+		is_prime = True
 
+
+if is_prime == True:
+	print('Prime')
+else:
+	print('Not prime')
