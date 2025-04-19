1f8ca7247aa07a09bc2dcf26b4c70ceae2b54a3b
rekha <rekhakirani@gmail.com>
2025-04-19 11:45:16 +0530

    Day 37 class

--- Day37(ClassVariables)/demo2.py
+++ Day37(ClassVariables)/demo2.py
@@ -1,7 +1,7 @@
 # Update class level variables
 class Electronics:
-    Product = "Mobile"
-    Store = "Croma"
+    Product = "Mobile" #class variable1
+    Store = "Croma" #class variable2
 
     def __init__(self,Name,Price,RAM,Color):
         self.name = Name
@@ -15,8 +15,11 @@ class Electronics:
 
 
     def change_ClassVariable(self,new_Name):
-        Electronics.Product = new_Name
+        Electronics.Product = new_Name #or self.Product = new_Name
 
+
+    def delete_ClassVariable(self):
+        del self.Product
 
 obj1 = Electronics("Samsung",20000,"6GB","White")
 obj1.call_ClassVariable()
@@ -30,5 +33,9 @@ obj1.call_ClassVariable()
 # Product Name: Laptop
 # Store Name: Croma
 
+# Delete class variable
+obj1.delete_ClassVariable()
 
+obj1.call_ClassVariable()
+# AttributeError: 'Electronics' object has no attribute 'Product'
 
